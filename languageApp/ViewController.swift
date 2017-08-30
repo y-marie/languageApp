eca3462997ba5b8ec45397e1040bdc2198f96fcf
yuki_mitsudome <everything.yu23@gmail.com>
2017-08-30 19:02:25 +0900

    Init commit

---
+++ languageApp/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  languageApp
+//
+//  Created by 有希 on 2017/06/03.
+//  Copyright © 2017年 Yuki Mitsudome. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func tapToTranslate(_ sender: UIButton) {
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+      
+    }
+
+
+}
+
---
+++ languageAppTests/yakuViewController.swift
@@ -0,0 +1,9 @@
+//
+//  yakuViewController.swift
+//  languageApp
+//
+//  Created by 有希 on 2017/08/30.
+//  Copyright © 2017年 Yuki Mitsudome. All rights reserved.
+//
+
+import Foundation
