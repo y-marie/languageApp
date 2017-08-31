d882bf255250a4e237d0c675266a8121412f2a92
yuki_mitsudome <everything.yu23@gmail.com>
2017-08-31 18:04:49 +0900

    Ui並べただけ

---
+++ languageApp/yakuViewController.swift
@@ -0,0 +1,25 @@
+//
+//  yakuViewController.swift
+//  languageApp
+//
+//  Created by 有希 on 2017/08/31.
+//  Copyright © 2017年 Yuki Mitsudome. All rights reserved.
+//
+
+import UIKit
+
+class yakuViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
